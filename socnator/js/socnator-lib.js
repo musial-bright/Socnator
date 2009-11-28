3990c0dec95b20756857abe7c8efb30400486043
Adam Musial-Bright <amusial@gmx.net>
2009-11-28 22:09:32 +0100

    ExtJS integrated into twitter view.

--- socnator/js/socnator-lib.js
+++ socnator/js/socnator-lib.js
@@ -1,81 +1,55 @@
 /* Adam Musial-Brihgt
  * 
- * Socnator (Social network aggregator)
- liobrary for presenting web servces content on one page. 
- * @require jQuery, Ext
+ * Socnator (SOCial Network aggregaTOR)
+ * Liobrary for network web services access.
+ *
+ * @require jQuery, ExtJS
  */
 (function() {
   
 	var config = {
 		"twitterImage" : true,
-		"twitterUserName" : "amusial"
+		"twitterUserName" : "amusial",
+		"twitterUserAttributes" : ["name", "description", "location"],
+		"userName" : "Adam Musial-Bright"
 	};
 
-
+  // tweets = [["Tweet1"],["Tweet2"]]
+  var tweets = Array();
 
   socnator = {
+
+	  getTweetsAsArray : function() { return tweets; },
+    
+    getUserName : function() { return config.userName; },
 	
-	  
-	  tweets : function() {
-	    return [
-        ['3m Co',71.72,0.02,0.03,'9/1 12:00am'],
-        ['Alcoa Inc',29.01,0.42,1.47,'9/1 12:00am'],
-        ['Altria <a href="http://www.google.de">google</a> Group Inc',83.81,0.28,0.34,'9/1 12:00am'],
-        ['American Express Company',52.55,0.01,0.02,'9/1 12:00am'],
-        ['American International Group, Inc.',64.13,0.31,0.49,'9/1 12:00am'],
-        ['AT&T Inc.',31.61,-0.48,-1.54,'9/1 12:00am'],
-        ['Boeing Co.',75.43,0.53,0.71,'9/1 12:00am'],
-        ['Caterpillar Inc.',67.27,0.92,1.39,'9/1 12:00am'],
-        ['Citigroup, Inc.',49.37,0.02,0.04,'9/1 12:00am'],
-        ['E.I. du Pont de Nemours and Company',40.48,0.51,1.28,'9/1 12:00am'],
-        ['Exxon Mobil Corp',68.1,-0.43,-0.64,'9/1 12:00am'],
-        ['General Electric Company',34.14,-0.08,-0.23,'9/1 12:00am'],
-        ['General Motors Corporation',30.27,1.09,3.74,'9/1 12:00am'],
-        ['Hewlett-Packard Co.',36.53,-0.03,-0.08,'9/1 12:00am'],
-        ['Honeywell Intl Inc',38.77,0.05,0.13,'9/1 12:00am'],
-        ['Intel Corporation',19.88,0.31,1.58,'9/1 12:00am'],
-        ['International Business Machines',81.41,0.44,0.54,'9/1 12:00am'],
-        ['Johnson & Johnson',64.72,0.06,0.09,'9/1 12:00am'],
-        ['JP Morgan & Chase & Co',45.73,0.07,0.15,'9/1 12:00am'],
-        ['McDonald\'s Corporation',36.76,0.86,2.40,'9/1 12:00am'],
-        ['Merck & Co., Inc.',40.96,0.41,1.01,'9/1 12:00am'],
-        ['Microsoft Corporation',25.84,0.14,0.54,'9/1 12:00am'],
-        ['Pfizer Inc',27.96,0.4,1.45,'9/1 12:00am'],
-        ['The Coca-Cola Company',45.07,0.26,0.58,'9/1 12:00am'],
-        ['The Home Depot, Inc.',34.64,0.35,1.02,'9/1 12:00am'],
-        ['The Procter & Gamble Company',61.91,0.01,0.02,'9/1 12:00am'],
-        ['United Technologies Corporation',63.26,0.55,0.88,'9/1 12:00am'],
-        ['Verizon Communications',35.57,0.39,1.11,'9/1 12:00am'],            
-        ['Wal-Mart Stores, Inc.',45.45,0.73,1.63,'9/1 12:00am']
-      ]
-    },
-	
+	  // Get your tweets from twitter.
     twitter : function() {
       var url = "http://twitter.com/statuses/user_timeline/" + 
-				config.twitterUserName + 
-				".json";
-				
+        config.twitterUserName + 
+        ".json";
+
       $.getJSON(url + "?callback=?", function(data){
+        var userInfoRendered = false;
         $.each(data, function(i, item) { 
-					if (config.twitterImage == true) {
-          	$("img#profile").attr("src", item.user["profile_image_url"]);
-        	}
-					$("#twitter ul").append(
-					  "<li>" +
-	          socnator.convertTextToLinks(item.text) + 
-	          " " +
-	          item.created_at +
-	          "</li>");
-          //return false;
+
+          if (userInfoRendered == false) {
+            if (config.twitterImage == true) {
+            	$("img#twitter_profile").attr("src", item.user['profile_image_url']);
+          	}
+          	
+            $.each(config['twitterUserAttributes'], function(i, name) {
+              $("#twitter_description").append(item.user[name] + "<br/>");
+            });
+            
+            userInfoRendered = true;
+          }
+          // date format = '9/1 12:00am'
+	        tweets.push([item.text, item.created_at]);
         });
       });
-    }, // eo:twitter
+    } // eo:twitter
 
-    convertTextToLinks : function(text) {
-			text = text.replace(/(#[a-zA-Z0-9:.-_]*)/g, '<i>$1</i>');
-			text = text.replace(/(http[a-zA-Z0-9:.-_]*)/g, '<a target="_blank" href="$1">$1</a>');
-      return text
-		}
   };
     
 }());
---
+++ socnator/js/socnator-twitter-view.js
@@ -0,0 +1,76 @@
+/* Adam Musial-Brihgt
+ * 
+ * Socnator twitter view
+ *
+ * @require jQuery, ExtJS
+ */
+Ext.onReady(function(){
+
+    // NOTE: This is an example showing simple state management. During development,
+    // it is generally best to disable state management as dynamically-generated ids
+    // can change across page loads, leading to unpredictable results.  The developer
+    // should ensure that stable state ids are set for stateful components in real apps.    
+    //Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+
+
+    // create the data store
+    var store = new Ext.data.ArrayStore({
+        fields: [
+           {
+             name: 'tweet',
+             type: 'string'
+           },
+           {
+             name: 'date',
+             type: 'date',
+             //created_at = "Mon Nov 09 21:25:56 +0000 2009"
+             dateFormat: 'D M d G:i:s O Y'
+            }
+        ]
+    });
+
+    // manually load local data
+    store.loadData(socnator.getTweetsAsArray());
+
+    function renderText(text){
+      text = text.replace(/(#[a-zA-Z0-9:.-_]*)/g, '<i>$1</i>');
+      text = text.replace(/(http[a-zA-Z0-9:.-_]*)/g, '<a target="_blank" href="$1">$1</a>');
+      text = String.format('<p style="white-space: normal">{0}</p>', text);
+      return text;
+    }
+
+    // create the Grid
+    var grid = new Ext.grid.GridPanel({
+      store: store,
+      stripeRows: true,
+      autoExpandColumn: 'tweet',
+      width: 400,
+      height: 350,
+      title: socnator.getUserName(),
+      // config options for stateful behavior
+      stateful: true,
+      stateId: 'grid',
+
+      columns: [
+          {
+            id:'tweet',
+            header: 'Tweets', 
+            width: 320, 
+            sortable: true,
+            renderer: renderText,
+            dataIndex: 'tweet'
+          },
+          {
+            header: 'Date', 
+            width: 80, 
+            sortable: true, 
+            renderer: Ext.util.Format.dateRenderer('M. d, Y'), 
+            dataIndex: 'date'}
+      ]
+
+    });
+    
+    
+    // render the grid to the specified div in the page
+    grid.render('tweets');
+});
